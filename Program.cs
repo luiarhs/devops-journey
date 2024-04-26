65db21c4c0c83664c186fe2a852799f07e18998e
luiarhs <luiarhs@yandex.com>
2024-04-26 00:02:21 -0600

    [add] db context

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ContactDBContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
