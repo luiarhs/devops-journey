4b523ab41aa075855aa9606d5c1dae2ea77f5510
luiarhs <luiarhs@yandex.com>
2024-04-28 23:26:34 -0600

    [add] healt checks

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Data; 
 using Services;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+    .AddCheck<StartupHealthCheck>(
+        "Startup",
+        tags: new[] { "ready" });
 
 var app = builder.Build();
 
@@ -27,5 +33,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/healthz");
+
+app.MapHealthChecks("/healthz/ready", new HealthCheckOptions
+{
+    Predicate = healthCheck => healthCheck.Tags.Contains("ready")
+});
+
+app.MapHealthChecks("/healthz/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
 
 app.Run();
