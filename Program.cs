dcc48bb99a31a5ea11f93e25aa2e3a35db0c5e73
luiarhs <luiarhs@yandex.com>
2024-04-28 23:45:42 -0600

    [add] services

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<StartupBackgroundService>();
+builder.Services.AddSingleton<StartupHealthCheck>();
 
 builder.Services.AddHealthChecks()
     .AddCheck<StartupHealthCheck>(
