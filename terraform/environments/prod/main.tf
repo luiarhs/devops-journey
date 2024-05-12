f56b130d8e7f54cde36b410d16af08a702c95664
luiarhs <luiarhs@yandex.com>
2024-05-11 23:49:00 -0600

    [add] network resources

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -8,6 +8,38 @@ resource "azurerm_resource_group" "main" {
   name     = "my-resource-group"
   location = "East US" # Choose your desired region
 }
+# Define virtual network named vnet0 with address space 10.9.0.0/16
+resource "azurerm_virtual_network" "vnet0" {
+  name                = "vnet0"
+  address_space       = ["10.9.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Define subnet for Azure bastion
+resource "azurerm_subnet" "azure_bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.0.0/24"]
+}
+
+# Define subnet for application
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.1.0/24"]
+}
+
+# Define subnet for database
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.2.0/24"]
+}
+
 
 # Define virtual network
 resource "azurerm_virtual_network" "main" {
@@ -163,6 +195,7 @@ resource "azurerm_public_ip" "lb_public_ip" {
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
 }
+
 
 # Define backend address pool for load balancer
 resource "azurerm_lb_backend_address_pool" "main" {
