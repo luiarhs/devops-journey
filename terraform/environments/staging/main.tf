7998545e4b44e59d08042019c73505b139a84874
luiarhs <luiarhs@yandex.com>
2024-05-14 23:41:26 -0600

    [add] subnets for bastion, app and db

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,4 +1,4 @@
-# Define virtual network named vnet0 with address space 10.9.0.0/16
+# 1. Define virtual network named vnet0 with address space 10.9.0.0/16
 resource "azurerm_virtual_network" "vnet0" {
   name                = "vnet0"
   address_space       = ["10.9.0.0/16"]
@@ -6,3 +6,26 @@ resource "azurerm_virtual_network" "vnet0" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
+# 2. Define subnet for Azure bastion
+resource "azurerm_subnet" "azure_bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.0.0/24"]
+}
+
+# Define subnet for application
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.1.0/24"]
+}
+
+# Define subnet for database
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet0.name
+  address_prefixes     = ["10.9.2.0/24"]
+}
