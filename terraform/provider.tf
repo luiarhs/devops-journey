d127e96b0aed18467929b7c28a8d3fbd00416cff
luiarhs <luiarhs@yandex.com>
2024-05-14 23:33:40 -0600

    [add] provider specs

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,9 +10,21 @@ terraform {
   }
 
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
     }
   }
 }
@@ -21,30 +33,3 @@ provider "azurerm" {
   features {}
   use_oidc = true
 }
-
-# terraform {
-#   required_version = ">=1.0"
-
-#   required_providers {
-#     azapi = {
-#       source  = "azure/azapi"
-#       version = "~>1.5"
-#     }
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~>3.0"
-#     }
-#     random = {
-#       source  = "hashicorp/random"
-#       version = "~>3.0"
-#     }
-#     time = {
-#       source  = "hashicorp/time"
-#       version = "0.9.1"
-#     }
-#   }
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
