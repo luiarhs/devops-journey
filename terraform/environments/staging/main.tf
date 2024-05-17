648cf2a342f82c49d336bdbe8bf5ebdba34c729e
luiarhs <luiarhs@yandex.com>
2024-05-16 23:45:39 -0600

    [add] virtual network

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,31 +1,16 @@
-# 1. Define virtual network named vnet0 with address space 10.9.0.0/16
-resource "azurerm_virtual_network" "vnet0" {
-  name                = "vnet0"
+#1 Define a Virtual Network
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-divelement"
+  location = "South Central US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
   address_space       = ["10.9.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-# 2. Define subnet for Azure bastion
-resource "azurerm_subnet" "azure_bastion_subnet" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet0.name
-  address_prefixes     = ["10.9.0.0/24"]
-}
-
-# Define subnet for application
-resource "azurerm_subnet" "app_subnet" {
-  name                 = "app-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet0.name
-  address_prefixes     = ["10.9.1.0/24"]
-}
-
-# Define subnet for database
-resource "azurerm_subnet" "db_subnet" {
-  name                 = "db-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet0.name
-  address_prefixes     = ["10.9.2.0/24"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
