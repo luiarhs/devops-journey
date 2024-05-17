961e5ce686537ed1a8744d35c7dae240915e5cc1
luiarhs <luiarhs@yandex.com>
2024-05-16 23:46:42 -0600

    [add] network interfaces defination

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -36,3 +36,28 @@ resource "azurerm_subnet" "db_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+#3 Define Network Interfaces
+resource "azurerm_network_interface" "app_vm_interface" {
+  name                = "app_vm_interface"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "app_vm_interface"
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "db_vm_interface" {
+  name                = "db_vm_interface"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "db_vm_interface"
+    subnet_id                     = azurerm_subnet.db_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
