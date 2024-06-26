9ebd88267fe13cf815b2a965e6ea88610efb6154
luiarhs <luiarhs@yandex.com>
2024-06-25 23:11:08 -0600

    [add] minor change

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,7 @@
 # Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
  backend "azurerm" {
+    resource_group_name   = "rg-terraform-state"
+    storage_account_name  = "stterraformstate"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
  } 
